599025790fab28c3518544b2ade95df7752cd919
Laurynas <laurynas.jastrizemskis@stud.svako.lt>
2026-05-16 21:48:05 +0300

    Sukūrtas prisijungimo ir registracijos puslapis

---
+++ views/login.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="lt">
+<head>
+    <meta charset="UTF-8">
+    <title>Prisijungimas</title>
+</head>
+<body>
+
+<h1>Prisijungimas</h1>
+
+<form method="POST">
+    <label>Prisijungimo vardas:</label><br>
+    <input type="text" name="vardas" required><br><br>
+
+    <label>Slaptažodis:</label><br>
+    <input type="password" name="slaptazodis" required><br><br>
+
+    <button type="submit">Prisijungti</button>
+</form>
+
+<p> Neturi susikūręs paskyros?<a href="index.php?page=register"> Registruotis čia</a> </p>
+
+</body>
+</html>
---
+++ views/register.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="lt">
+<head>
+    <meta charset="UTF-8">
+    <title>Registracija</title>
+</head>
+<body>
+
+<h1>Registracija</h1>
+
+<form method="POST">
+    <label>Vartotojo vardas:</label><br>
+    <input type="text" name="vardas" required><br><br>
+
+    <label>Slaptažodis:</label><br>
+    <input type="password" name="slaptazodis" required><br><br>
+
+    <button type="submit">Registruotis</button>
+</form>
+
+<p> Turi paskyrą? <a href="index.php?page=login"> Prisijungti čia </a> </p>
+
+</body>
+</html>
